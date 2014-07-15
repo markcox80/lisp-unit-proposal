3268afa0917aab8e405f5768ac954220f0a7bbac
Mark Cox <markcox80@gmail.com>
2014-07-15 10:17:02 +1000

    Un-commented the ASDF:TEST-OP for the LISP-UNIT version of the tests for MUSIC-PLAYER.

--- mp/tests/asdf.lisp
+++ mp/tests/asdf.lisp
@@ -1,13 +1,7 @@
 (in-package "MUSIC-PLAYER.TESTS")
 
-#+:lisp-unit
 (defmethod asdf:perform ((op asdf:test-op) (cmp (eql (asdf:find-system "music-player"))))
   (dolist (pkg (list "MUSIC-PLAYER.CONTROLLER.TESTS"
 		     "MUSIC-PLAYER.CODEC.TESTS"))
     (format t ";; Running tests in package: ~A~%" pkg)
     (lisp-unit:run-tests :all pkg)))
-
-#+:lisp-unit-proposal
-(defmethod asdf:perform ((op asdf:test-op) (cmp (eql (asdf:find-system "music-player-proposal"))))
-  (run-test-collections "MUSIC-PLAYER.CONTROLLER.TESTS"
-			"MUSIC-PLAYER.CODEC.TESTS"))
