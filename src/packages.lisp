f4b1e147cfcd769e1164030b71910806209bee30
Mark Cox <markcox80@gmail.com>
2014-07-15 10:26:35 +1000

    Export symbols from LISP-UNIT-PROPOSAL package.

--- src/packages.lisp
+++ src/packages.lisp
@@ -1,2 +1,28 @@
 (defpackage "LISP-UNIT-PROPOSAL"
-  (:use "COMMON-LISP"))
+  (:use "COMMON-LISP")
+  
+  ;; Tests
+  (:export #:define-test)
+
+  ;; Assertions
+  (:export #:assert-true)
+
+  ;; Results
+  (:export #:print-summary
+	   #:print-errors)
+
+  ;; Test Filters
+  (:export #:define-test-filter
+	   #:compile-test-filter
+	   #:expand-test-filter
+	   #:expand-test-filter-1)
+
+  ;; Test Selections
+  (:export #:define-test-selection
+	   #:compile-test-selection
+	   #:expand-test-selection
+	   #:expand-test-selection-1)
+
+  ;; Execution
+  (:export #:run-tests
+	   #:run-test-selection))
